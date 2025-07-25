0648ecaf760af4b5e575b086d0b541f40475a4f8
Heisenbait <joaquinalonsosalgadobv@gmail.com>
2025-07-25 13:12:27 -0500

    script.js

--- hsnbrg01/script.js
+++ hsnbrg01/script.js
@@ -79,6 +79,78 @@ albumImage.addEventListener('mouseleave', () => {
       duration: "4:16"
     },
     {
+      src: "archivos/Moon - Kanye West.mp3",
+      name: "Moon",
+      image: "archivos/Cover of Moon by Kanye West.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Count Me Out - Kendrick Lamar.mp3",
+      name: "Count Me Out",
+      image: "archivos/Cover of Count Me Out by Kendrick Lamar.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Facts (Charlie Heat Version) - Kanye West.mp3",
+      name: "Facts",
+      image: "archivos/Cover of Father Stretch My Hands Pt. 1 by Kanye West.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/BEST INTEREST - Tyler.mp3",
+      name: "BEST INTEREST",
+      image: "archivos/Cover of BEST INTEREST by Tyler, The Creator.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Dontmakemefallinlove - Cuco.mp3",
+      name: "Dontmakemefallinlove",
+      image: "archivos/Cover of Dontmakemefallinlove by Cuco.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Know Yourself - Drake.mp3",
+      name: "Know Yourself",
+      image: "archivos/Cover of Know Yourself by Drake.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Estrelar - Marcos Valle.mp3",
+      name: "Estrelar",
+      image: "archivos/Cover of Estrelar by Marcos Valle.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/DUCKWORTH. - Kendrick Lamar.mp3",
+      name: "DUCKWORTH.",
+      image: "archivos/Cover of DUCKWORTH. by Kendrick Lamar.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Tristes Ojos - Ramona.mp3",
+      name: "Tristes Ojos",
+      image: "archivos/Cover of Tristes Ojos by Ramona.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Looking Out for You - Joy Again.mp3",
+      name: "Looking Out for You",
+      image: "archivos/Cover of Looking Out for You by Joy Again.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Melting - Kali Uchis.mp3",
+      name: "Melting",
+      image: "archivos/Cover of Melting by Kali Uchis.jpg",
+      duration: "2:39"
+    },
+    {
+      src: "archivos/Do Better - Cuco.mp3",
+      name: "Do Better",
+      image: "archivos/Cover of Do Better by Cuco.jpg",
+      duration: "2:39"
+    },
+    {
       src: "archivos/BURN - Kanye West.mp3",
       name: "BURN",
       image: "archivos/Cover of PAPERWORK by Kanye West, Ty Dolla $ign.jpg",
