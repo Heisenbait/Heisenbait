21a3bed1b6a2c335693df5fba85c59c1dd914a5f
Heisenbait <joaquinalonsosalgadobv@gmail.com>
2025-08-20 00:27:37 -0500

    script.js

--- hsnbrg01/script.js
+++ hsnbrg01/script.js
@@ -150,7 +150,7 @@ albumImage.addEventListener('mouseleave', () => {
     {
       src: "archivos/Lift Yourself - Kanye West.mp3",
       name: "Lift Yourself",
-      image: "archivos/Cover of Lift Yourself by Kanye West.jpg",
+      image: "archivos/Cover of Paranoid by Kanye West, Mr Hudson.jpg",
       duration: "2:27"
     },
     {
