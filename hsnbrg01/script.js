a7c77e88804f05cb883e16253f8c782a69ab12b1
Heisenbait <joaquinalonsosalgadobv@gmail.com>
2025-09-09 12:03:41 -0500

    script.js

--- hsnbrg01/script.js
+++ hsnbrg01/script.js
@@ -98,7 +98,7 @@ albumImage.addEventListener('mouseleave', () => {
       name: "Run Your Mouth",
       image: "archivos/Cover of Run Your Mouth by The Marías.jpg",
       duration: "2:40",
-      text: "The Marías" 
+      text: "The Marías"
     },
     {
       src: "archivos/Deja Vu - Gustavo Cerati.mp3",
