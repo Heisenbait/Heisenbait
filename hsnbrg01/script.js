10e94887181cc2fc2a90136361337a39bf80f2be
Heisenbait <joaquinalonsosalgadobv@gmail.com>
2026-09-04 01:54:53 -0500

    Update script.js

--- hsnbrg01/script.js
+++ hsnbrg01/script.js
@@ -305,6 +305,27 @@ albumImage.addEventListener('mouseleave', () => {
       text: "Kanye West"
     },
     {
+      src: "archivos/$, Ye, Ty Dolla $ign - Everything ft. Ant Clemons.mp3",
+      name: "Everything",
+      image: "archivos/BBPB cover.png",
+      duration: "1:45",
+      text: "Kanye West, Ty Dolla $ign, Ant Clemons"
+    },
+    {
+      src: "archivos/$, Ye, Ty Dolla $ign - Worship.mp3",
+      name: "Worship",
+      image: "archivos/BBPB cover.png",
+      duration: "1:45",
+      text: "Kanye West, Ty Dolla $ign"
+    },
+    {
+      src: "archivos/$, Ye, Ty Dolla $ign, Project Pat, Young Moose - PAY PER VIEW.mp3",
+      name: "PAY PER VIEW",
+      image: "archivos/BBPB cover.png",
+      duration: "1:45",
+      text: "Kanye West, Ty Dolla $ign, Young Moose"
+    },
+    {
       src: "archivos/MY HOOD - Kanye West.mp3",
       name: "MY HOOD",
       image: "archivos/Bully-edit.jpeg",
